00ee76831c069f6150ad7782beca87c0b97565ae
hechengyao <hechengyao@bonc.com.cn>
2021-07-21 11:18:28 +0800

    first commit

---
+++ src/main/java/com/dokstudio/springboot2/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.dokstudio.springboot2.controller;
+
+/**
+ * @author Jiuzhoumu
+ * @date 2021/7/21 11:17
+ * @projectName springboot2
+ * @title: UserController
+ * @description: TODO
+ */
+public class UserController {
+}
