60e2a1345a76e6f266a65ca80036b56be031f0b7
hechengyao <hechengyao@bonc.com.cn>
2021-08-24 16:33:19 +0800

    Commit:   丰富 controller 层

--- src/main/java/com/dokstudio/springboot2/controller/UserController.java
+++ src/main/java/com/dokstudio/springboot2/controller/UserController.java
@@ -1,5 +1,10 @@
 package com.dokstudio.springboot2.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Jiuzhoumu
  * @date 2021/7/21 11:17
@@ -7,5 +12,15 @@ package com.dokstudio.springboot2.controller;
  * @title: UserController
  * @description: TODO
  */
+@RestController
 public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @RequestMapping("/huge.png")
+    public String huge() {
+        logger.info(">>>>>> huge");
+        return "huge";
+    }
+
 }
