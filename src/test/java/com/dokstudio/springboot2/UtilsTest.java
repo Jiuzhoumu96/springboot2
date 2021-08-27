6cd29fa76b42ed63042dae52db4058a316a3f128
hechengyao <hechengyao@bonc.com.cn>
2021-08-27 09:46:31 +0800

    Modify:   优化 cpuUsage 的日志

---
+++ src/test/java/com/dokstudio/springboot2/UtilsTest.java
@@ -0,0 +1,23 @@
+package com.dokstudio.springboot2;
+
+import com.dokstudio.springboot2.utils.OSUtils;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author: HeChengyao
+ * @date: 2021/8/27 9:11
+ */
+public class UtilsTest {
+
+    private static final Logger logger=LoggerFactory.getLogger(UtilsTest.class);
+
+    @Test
+    public static void main(String[] args) {
+
+        System.out.println(OSUtils.cpuUsage());
+
+    }
+
+}
